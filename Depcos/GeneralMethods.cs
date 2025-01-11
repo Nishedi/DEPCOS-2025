39487b8a6f3bc226f485687d9a3611c939eb47d8
Nishedi <263948@student.pwr.edu.pl>
2025-01-11 17:30:38 +0100

    algorytm TS

---
+++ Depcos/GeneralMethods.cs
@@ -0,0 +1,140 @@
+﻿using Depcos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class GeneralMethods
+{
+    public List<Customer> createGreedyGTR(double[,] distanceMatrix, List<Customer> Customers, List<double> vehicleStarts, List<Vehicle> Vehicles)
+    {
+        var InitialGTR = new List<Customer>();
+        bool[] visited = new bool[Customers.Count];
+        int vehicleNumber = 0;
+        double vehicleTime = 0;
+
+        // Każdy pojazd zaczyna z bazy
+        InitialGTR.Add(Customers[0]);
+
+        while (visited.Contains(false))
+        {
+            Customer current = Customers[0]; // Start z bazy
+            visited[current.Id] = true;
+            vehicleTime = 0;
+
+            while (true)
+            {
+                Customer nextCustomer = null;
+                double minDistance = double.MaxValue;
+
+                // Szukaj najbliższego klienta, który spełnia warunki
+                foreach (var customer in Customers)
+                {
+                    if (!visited[customer.Id] && customer.Id != 0)
+                    {
+                        double distance = distanceMatrix[current.Id, customer.Id]; // koszt przejazdu do punktu
+                        double estimatedUpperTimeLeft = customer.dv - vehicleTime; // koszt przekroczenia okna z gory
+                        double estimatedLowerTimeLeft = customer.bv - vehicleTime; // koszt przekroczenia okna z dolu
+                        double estimatedPenalty = Math.Max(0, customer.ServiceTime - estimatedUpperTimeLeft);
+                        estimatedPenalty += Math.Max(0, Math.Min(estimatedLowerTimeLeft, customer.ServiceTime));
+                        distance += estimatedPenalty;
+                        // Pomyslec o rozbudowaniu warunku krytycznego
+                        if (distance < minDistance && distance + vehicleTime < Customers[0].dv)
+                        {
+                            minDistance = distance;
+                            nextCustomer = customer;
+                        }
+                    }
+                }
+
+                // Jeśli nie ma dostępnych klientów, wróć do bazy
+                if (nextCustomer == null && current.Id != 0 || vehicleTime >= Customers[0].dv)
+                {
+                    InitialGTR.Add(Customers[0]); // Powrót do bazy
+                    vehicleNumber++;
+                    if (vehicleNumber >= Vehicles.Count)
+                    {
+                        return InitialGTR;
+                    }
+                    break;
+                }
+                else if (nextCustomer == null && current.Id == 0) // Jeżeli ścieżka jest pusta i nie znaleziono klienta który spełnia wymagania
+                                                                  // np. nie da się zdążyć do końca okna czasowego
+                                                                  // dodaj pierwsze nieodwiedzone miasto
+                {
+                    foreach (var customer in Customers)
+                    {
+                        if (!visited[customer.Id])
+                        {
+                            nextCustomer = customer;
+                        }
+                    }
+                }
+
+                if (current.Id == 0)
+                {
+                    vehicleTime = Math.Max(nextCustomer.bv - distanceMatrix[current.Id, nextCustomer.Id], 0);
+                    vehicleStarts.Add(Math.Max(nextCustomer.bv - distanceMatrix[current.Id, nextCustomer.Id], 0.0));
+                }
+                vehicleTime += distanceMatrix[current.Id, nextCustomer.Id]; // Przesuń czas o czas podróży
+                // Odwiedź wybranego klienta
+                double upperTimeLeft = nextCustomer.dv - vehicleTime; // dodaktowy koszt za przekroczenie od gory okna
+                double lowerTimeLeft = nextCustomer.bv - vehicleTime; // dodatkowy koszt za przekroczenie od dolu okna
+                double penalty = Math.Max(0, nextCustomer.ServiceTime - upperTimeLeft); // naliczenie potencjalnej kary
+                penalty += Math.Max(0, Math.Min(lowerTimeLeft, nextCustomer.ServiceTime));
+                vehicleTime += nextCustomer.ServiceTime; // Wykonaj serwis
+                vehicleTime += penalty;
+                InitialGTR.Add(nextCustomer); // Odznacz punkt
+                visited[nextCustomer.Id] = true;
+                //Console.WriteLine(nextCustomer.Id + " " + vehicleTime);
+                current = nextCustomer;
+            }
+        }
+
+        // Zakończ trasę powrotem do bazy (jeśli ostatni pojazd jest w trasie)
+        if (InitialGTR[InitialGTR.Count - 1].Id != 0)
+        {
+            InitialGTR.Add(Customers[0]);
+        }
+
+        return InitialGTR;
+    }
+
+    public double calculateCostGTRv2(double[,] distanceMatrix, List<Customer> GTR, List<Double> vehicleStartTimes)
+    {
+        double cost = 0;
+        double vehicleTime = 0;
+        Customer prevCustomer = null;
+        int count = GTR.Count(customer => customer.Id == 0);
+        int vehicleId = 0;
+        foreach (Customer customer in GTR)
+        {
+            if (customer.Id == 0)
+            {
+                if (prevCustomer != null)
+                {
+                    vehicleTime += distanceMatrix[prevCustomer.Id, GTR[0].Id];
+                    cost += vehicleTime - vehicleStartTimes[vehicleId];
+                    vehicleId++;
+                }
+                //Console.WriteLine(vehicleTime); // wyswietlenie kosztu dla trasy pojedynczych pojazdow
+                if (vehicleId < vehicleStartTimes.Count)
+                    vehicleTime = vehicleStartTimes[vehicleId];
+                prevCustomer = customer;
+            }
+            else
+            {
+                vehicleTime += distanceMatrix[prevCustomer.Id, customer.Id];
+                double upperTimeLeft = customer.dv - vehicleTime; // dodaktowy koszt za przekroczenie od gory okna
+                double lowerTimeLeft = customer.bv - vehicleTime; // dodatkowy koszt za przekroczenie od dolu okna
+                double penalty = Math.Max(0, customer.ServiceTime - upperTimeLeft); // naliczenie potencjalnej kary
+                penalty += Math.Max(0, Math.Min(lowerTimeLeft, customer.ServiceTime));
+                vehicleTime += customer.ServiceTime; // Wykonaj serwis
+                vehicleTime += penalty;
+                prevCustomer = customer;
+            }
+        }
+        return cost;
+    }
+}
---
+++ Depcos/TS.cs
@@ -0,0 +1,225 @@
+﻿using Depcos;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+public class TS
+{
+    private void Reverse(List<Customer> list, int start, int end)
+    {
+        while (start < end)
+        {
+            (list[start], list[end]) = (list[end], list[start]);
+            start++;
+            end--;
+        }
+    }
+
+    public List<Neighbor> GenerateNeighbor1_X(double[,] distances, List<Customer> GTR, int numberOfMaxMoves, int seed)
+    {
+        var random = new Random(seed);
+        var neighborhood = new List<Neighbor>();
+        //&& iter < 1000000
+
+        for (int iter = 0; iter < numberOfMaxMoves ; iter++)
+        {
+            var vehicleStartTimes = new List<double>();
+            int i = random.Next(GTR.Count-2)+1;
+            int j;
+
+            do
+            {
+                j = random.Next(GTR.Count - 2) + 1;
+            } while (i == j);
+
+            Customer valueToMove = GTR[i];
+            var neighbor = new List<Customer>(GTR);
+            neighbor.RemoveAt(i);
+            neighbor.Insert(j, valueToMove);
+            for(int x = 0; x < neighbor.Count;x++)
+            {
+                if (neighbor[x].Id == 0 && x+1<neighbor.Count)
+                {
+                    vehicleStartTimes.Add(Math.Max(neighbor[x + 1].bv - distances[neighbor[x].Id, neighbor[x + 1].Id],0));
+                }
+            }
+            neighborhood.Add(new Neighbor(neighbor,vehicleStartTimes, i, j));
+        }
+
+        return neighborhood;
+    }
+    public List<Neighbor> GenerateNeighborInsert(double[,] distances, List<Customer> GTR, int numberOfMaxMoves, int seed)
+    {
+        var random = new Random(seed);
+        var neighborhood = new List<Neighbor>();
+        //&& iter < 1000000
+
+        for (int i = 1; i < GTR.Count - 1; i++)
+        {
+            for(int j = 1; j < GTR.Count-1; j++)
+            {
+                if (i == j) continue;
+                var vehicleStartTimes = new List<double>();
+                Customer valueToMove = GTR[i];
+                var neighbor = new List<Customer>(GTR);
+                neighbor.RemoveAt(i);
+                neighbor.Insert(j, valueToMove);
+                for (int x = 0; x < neighbor.Count; x++)
+                {
+                    if (neighbor[x].Id == 0 && x + 1 < neighbor.Count)
+                    {
+                        vehicleStartTimes.Add(Math.Max(neighbor[x + 1].bv - distances[neighbor[x].Id, neighbor[x + 1].Id], 0));
+                    }
+                }
+                neighborhood.Add(new Neighbor(neighbor, vehicleStartTimes, i, j));
+            }
+        }
+
+        return neighborhood;
+    }
+    public List<Neighbor> GenerateNeighborSwap(double[,] distances, List<Customer> GTR, int numberOfMaxMoves, int seed)
+    {
+        var neighborhood = new List<Neighbor>();
+        //&& iter < 1000000
+
+        for (int i = 1; i < GTR.Count - 1; i++)
+        {
+            for (int j = 1; j < GTR.Count - 1; j++)
+            {
+
+                if (i == j) continue;
+                var vehicleStartTimes = new List<double>();
+                var neighbor = new List<Customer>(GTR);
+                (neighbor[i], neighbor[j]) = (neighbor[j], neighbor[i]);
+                for (int x = 0; x < neighbor.Count; x++)
+                {
+                    if (neighbor[x].Id == 0 && x + 1 < neighbor.Count)
+                    {
+                        vehicleStartTimes.Add(Math.Max(neighbor[x + 1].bv - distances[neighbor[x].Id, neighbor[x + 1].Id], 0));
+                    }
+                }
+                neighborhood.Add(new Neighbor(neighbor, vehicleStartTimes, i, j));
+
+            }
+        }
+        return neighborhood;
+    }
+
+    public List<Neighbor> GenerateNeighborReverse(double[,] distances, List<Customer> GTR, int numberOfMaxMoves, int seed)
+    {
+        var neighborhood = new List<Neighbor>();
+        //&& iter < 1000000
+
+        for (int i = 1; i < GTR.Count - 1; i++)
+        {
+            for (int j = 1; j < GTR.Count - 1; j++)
+            {
+                if (i == j) continue;
+                var vehicleStartTimes = new List<double>();
+                var neighbor = new List<Customer>(GTR);
+                Reverse(neighbor, i, j);
+                for (int x = 0; x < neighbor.Count; x++)
+                {
+                    if (neighbor[x].Id == 0 && x + 1 < neighbor.Count)
+                    {
+                        vehicleStartTimes.Add(Math.Max(neighbor[x + 1].bv - distances[neighbor[x].Id, neighbor[x + 1].Id], 0));
+                    }
+                }
+                neighborhood.Add(new Neighbor(neighbor, vehicleStartTimes, i, j));
+            }
+        }
+        return neighborhood;
+    }
+
+   
+    public Result TabuSearch(double[,] distances, List<Customer> customers, List<Vehicle> vehicles, int tabuSize, int maxTime,  int neighborType)
+    {
+        var bestSolution = new List<Customer>();
+        double bestSolutionLength = int.MaxValue;
+        var gm = new GeneralMethods();
+        var currentSolution = new List<Customer>();
+        List<double> vehicleStarts = new List<double>();
+        List<double> bestVehicleStarts = new List<double>();
+        currentSolution = gm.createGreedyGTR(distances, customers, vehicleStarts, vehicles);
+        double currentSolutionLength = gm.calculateCostGTRv2(distances, currentSolution, vehicleStarts);
+
+        if (bestSolution == null || currentSolutionLength < bestSolutionLength)
+        {
+            bestSolution = currentSolution;
+            bestSolutionLength = currentSolutionLength;
+            bestVehicleStarts = vehicleStarts;
+        }
+
+        maxTime *= 1000;
+        var tabuList = new List<Neighbor>();
+        var stopwatch = Stopwatch.StartNew();
+        currentSolution = bestSolution;
+        while (stopwatch.ElapsedMilliseconds <= maxTime)
+        {
+            List<Neighbor> neighborhood;
+            if (neighborType == 1)
+                neighborhood = GenerateNeighborInsert(distances, currentSolution, currentSolution.Count, 1000);
+            else if (neighborType == 2)
+                neighborhood = GenerateNeighborSwap(distances, currentSolution, currentSolution.Count, 1000);
+            else
+                neighborhood = GenerateNeighborReverse(distances, currentSolution, currentSolution.Count,1000);
+
+            neighborhood.Sort((a, b) => gm.calculateCostGTRv2(distances,a.Path,a.VehicleStarts).CompareTo(gm.calculateCostGTRv2(distances,b.Path,b.VehicleStarts)));
+            foreach (var neighbor in neighborhood)
+            {
+                if (!tabuList.Contains(neighbor) || gm.calculateCostGTRv2(distances, neighbor.Path, neighbor.VehicleStarts) < bestSolutionLength)
+                {
+                    currentSolution = neighbor.Path;
+                    vehicleStarts = neighbor.VehicleStarts;
+                    tabuList.Add(neighbor);
+                    if (tabuList.Count > tabuSize)
+                    {
+                        tabuList.RemoveAt(0);
+                    }
+                    break;
+                }
+            }
+
+            currentSolutionLength = gm.calculateCostGTRv2(distances, currentSolution, vehicleStarts);
+
+            if (currentSolutionLength < bestSolutionLength)
+            {
+                bestSolution = currentSolution;
+                bestSolutionLength = currentSolutionLength;
+                bestVehicleStarts = vehicleStarts;
+            }
+        }
+        return new Result(bestSolution, vehicleStarts);
+    }
+}
+
+public class Neighbor
+{
+    public int I { get; set; }
+    public int J { get; set; }
+    public List<Customer> Path { get; set; }
+    public List<Double> VehicleStarts { get; set; }
+
+    public Neighbor(List<Customer> path, List<Double> vehicleStarts, int i, int j)
+    {
+        Path = path;
+        VehicleStarts = vehicleStarts;
+        I = i;
+        J = j;
+    }
+
+    public override bool Equals(object obj)
+    {
+        return obj is Neighbor neighbor &&
+               I == neighbor.I &&
+               J == neighbor.J &&
+               Path.SequenceEqual(neighbor.Path);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(I, J, Path);
+    }
+}
