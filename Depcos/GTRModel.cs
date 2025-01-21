82e764b102382a936495e7156c724d5a7956b811
Nishedi <263948@student.pwr.edu.pl>
2025-01-21 17:11:22 +0100

    Poczatki genetyka

---
+++ Depcos/GTRModel.cs
@@ -0,0 +1,129 @@
+﻿using Gurobi;
+using System;
+using System.Collections.Generic;
+
+public class GTRModel
+{
+    public double CalculateCostGTRv2(double[,] distanceMatrix, List<Customer> GTR, List<Double> vehicleStartTimes)
+    {
+        try
+        {
+            // Inicjalizacja modelu Gurobi
+            using (var env = new GRBEnv(true))
+            {
+                env.Start();
+                using (var model = new GRBModel(env))
+                {
+                    int numCustomers = GTR.Count;
+                    int numVehicles = vehicleStartTimes.Count;
+
+                    // Zmienne decyzyjne
+                    GRBVar[,] x = new GRBVar[numCustomers, numCustomers];
+                    GRBVar[] t = new GRBVar[numCustomers];
+
+                    // Zmienna binarna x[i, j] dla każdej pary klientów
+                    for (int i = 0; i < numCustomers; i++)
+                    {
+                        for (int j = 0; j < numCustomers; j++)
+                        {
+                            if (i != j)
+                            {
+                                x[i, j] = model.AddVar(0.0, 1.0, 0.0, GRB.BINARY, $"x_{i}_{j}");
+                            }
+                        }
+                    }
+
+                    // Zmienna t[i] dla czasów przyjazdu do klienta i
+                    for (int i = 0; i < numCustomers; i++)
+                    {
+                        t[i] = model.AddVar(0.0, GRB.INFINITY, 0.0, GRB.CONTINUOUS, $"t_{i}");
+                    }
+
+                    // Ograniczenie: każdy klient musi być odwiedzony dokładnie raz
+                    for (int i = 0; i < numCustomers; i++)
+                    {
+                        GRBLinExpr lhs = 0;
+                        for (int j = 0; j < numCustomers; j++)
+                        {
+                            if (i != j)
+                            {
+                                lhs += x[i, j];
+                            }
+                        }
+                        model.AddConstr(lhs == 1, $"visit_once_{i}");
+                    }
+
+                    // Ograniczenie: każdy pojazd może wyjechać z dokładnie jednego klienta
+                    for (int j = 0; j < numCustomers; j++)
+                    {
+                        GRBLinExpr lhs = 0;
+                        for (int i = 0; i < numCustomers; i++)
+                        {
+                            if (i != j)
+                            {
+                                lhs += x[i, j];
+                            }
+                        }
+                        model.AddConstr(lhs == 1, $"depart_once_{j}");
+                    }
+
+                    // Czas przyjazdu ograniczenie (t[i] + czas przejazdu <= t[j] + M * (1 - x[i, j]))
+                    double M = 1000000.0; // duża liczba, na przykład 1e6
+                    for (int i = 0; i < numCustomers; i++)
+                    {
+                        for (int j = 0; j < numCustomers; j++)
+                        {
+                            if (i != j)
+                            {
+                                model.AddConstr(t[i] + distanceMatrix[i, j] <= t[j] + M * (1 - x[i, j]), $"time_constraint_{i}_{j}");
+                            }
+                        }
+                    }
+
+                    // Ograniczenia czasów dla każdego pojazdu, biorąc pod uwagę start czasów pojazdów
+                    for (int i = 0; i < numCustomers; i++)
+                    {
+                        model.AddConstr(t[i] >= vehicleStartTimes[i], $"vehicle_start_{i}");
+                    }
+
+                    // Funkcja celu: minimalizacja kosztów przejazdów i kar
+                    GRBLinExpr objective = 0;
+                    for (int i = 0; i < numCustomers; i++)
+                    {
+                        for (int j = 0; j < numCustomers; j++)
+                        {
+                            if (i != j)
+                            {
+                                objective += distanceMatrix[i, j] * x[i, j];
+                            }
+                        }
+                    }
+
+                    // Dodanie funkcji celu do modelu
+                    model.SetObjective(objective, GRB.MINIMIZE);
+
+                    // Optymalizacja
+                    model.Optimize();
+
+                    // Wyniki
+                    if (model.Status == GRB.Status.OPTIMAL)
+                    {
+                        double totalCost = model.ObjVal;
+                        Console.WriteLine("Optymalny koszt: " + totalCost);
+                        return totalCost;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Brak rozwiązania optymalnego");
+                        return double.MaxValue;
+                    }
+                }
+            }
+        }
+        catch (GRBException e)
+        {
+            Console.WriteLine("Gurobi error: " + e.Message);
+            return double.MaxValue;
+        }
+    }
+}
---
+++ Depcos/Genetic.cs
@@ -0,0 +1,131 @@
+﻿using Depcos;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+public class Genetic
+{
+    private int distancesSize;
+    private int populationSize = 10000;
+    private GeneralMethods gm = new GeneralMethods();
+    Random generator = new Random();
+
+
+    private List<Result> InitializePopulation(double[,] distancesMatrix, List<Customer> customers, List<Vehicle> vehicles)
+    {
+        var population = new List<Result>(this.populationSize);
+
+        List<double> vehicleStarts = new List<double>();
+        var GTR = gm.createGreedyGTR(distancesMatrix, customers, vehicleStarts, vehicles);
+        population.Add(new Result(GTR, vehicleStarts));
+
+
+        for (int i = distancesSize; i < this.populationSize; i++)
+        {
+            vehicleStarts = new List<double>();
+            GTR = gm.createGreedyGTRMutated(distancesMatrix, customers, vehicleStarts, vehicles);
+            population.Add(new Result(GTR, vehicleStarts));
+        }
+        return population;
+    }
+
+    private Result SelectParent(double[,] distancesMatrix, List<Result> population)
+    {
+        int tournamentSize = population[0].GTR.Count / 2; // pierwotnie 2
+        var tournament = new List<Result>();
+
+        for (int i = 0; i < tournamentSize; i++)
+        {
+            int randomIndex = generator.Next(population.Count);
+            tournament.Add(population[randomIndex]);
+        }
+
+        int bestTourIndex = 0;
+        double bestTourDistance = gm.calculateCostGTRv2(distancesMatrix, tournament[0].GTR, tournament[0].VehicleStartTimes);
+
+        for (int i = 1; i < tournamentSize; i++)
+        {
+            double distance = gm.calculateCostGTRv2(distancesMatrix, tournament[i].GTR, tournament[i].VehicleStartTimes);
+
+            if (distance < bestTourDistance)
+            {
+                bestTourDistance = distance;
+                bestTourIndex = i;
+            }
+        }
+
+        return tournament[bestTourIndex];
+    }
+
+    public Result GeneticSolve(double[,] distances, List<Customer> customers, List<Vehicle> vehicles, int maxTime)
+    {
+        maxTime *= 1000;
+        
+        double bestDistance = int.MaxValue;
+        
+        
+        var bestTour = new Result(null, null);
+
+        Stopwatch stopwatch = Stopwatch.StartNew();
+        var population = InitializePopulation(distances, customers, vehicles);
+
+
+        while (stopwatch.ElapsedMilliseconds <= maxTime)
+        {
+            var newPopulation = new List<Result>(populationSize);
+
+            for (int i = 0; i < populationSize; i++)
+            {
+                var first = SelectParent(distances, population);
+                var second = SelectParent(distances, population);
+
+                /*int iter = 0;
+                while (iter++ < 100 && first.SequenceEqual(second))
+                {
+                    second = SelectParent(population);
+                }
+
+                List<int> child;
+                if (first.SequenceEqual(second))
+                {
+                    child = cs.PMXCrossOver(first, second, distancesSize, 0);
+                }
+                else
+                {
+                    child = crossoverType switch
+                    {
+                        0 => cs.PMXCrossOver(first, second, distancesSize, crossoverRate),
+                        1 => cs.OrderCrossover(first, second, distancesSize, crossoverRate),
+                        _ => throw new ArgumentException("Invalid crossover type")
+                    };
+                }
+
+                if (mutationType == 0) mt.InsertionMutate(child, distancesSize, mutationRate);
+                if (mutationType == 1) mt.SwapMutate(child, distancesSize, mutationRate);*/
+                var child = first;
+
+                newPopulation.Add(child);
+            }
+
+            double lastBest = bestDistance;
+            foreach (var individual in newPopulation)
+            {
+                double distance = gm.calculateCostGTRv2(distances, individual.GTR, individual.VehicleStartTimes);
+
+                if (distance < bestDistance)
+                {
+                    bestDistance = distance;
+                    bestTour = individual;
+                }
+
+                if (distance < 2.5 * lastBest)
+                {
+                    int randomIndex = generator.Next(population.Count);
+                    population[randomIndex] = individual;
+                }
+            }
+        }
+
+        return bestTour;
+    }
+}
